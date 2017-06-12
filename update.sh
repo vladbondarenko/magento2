364b22029fceb2d12f1b80fbaa9cd33f34245cf8
root <root@ip-172-31-6-118.eu-central-1.compute.internal>
2017-06-12 09:13:04 +0000

    2.1.7

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-su -s /bin/bash -c "./composer require magento/product-community-edition 2.1.6 --no-update" nginx
+su -s /bin/bash -c "./composer require magento/product-community-edition 2.1.7 --no-update" nginx
 su -s /bin/bash -c "./composer update" nginx
 su -s /bin/bash -c "rm -rf var/di var/generation" nginx
 su -s /bin/bash -c "php bin/magento cache:flush" nginx
